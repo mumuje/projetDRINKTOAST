a734f7dd2a6464f5e764160013188909b99820e3
Tisma <matisagr@gmail.com>
2023-12-18 00:27:20 +0100

    Update font and styles in CSS files

--- index.php
+++ index.php
@@ -6,25 +6,27 @@
   <title>DrinkToast | Accueil</title>
   <link rel="icon" href="img/logo.png" type="image/png">
 
-  <!-- Votre style CSS -->
-  <?php include 'include_css.php'; ?>
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- Votre style CSS -->
+  <?php include 'include_css.php'; ?>
 
 
 </head>
 
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-gold">
+  <nav class="navbar navbar-expand-lg" style="background-color: var(--dark-color)">
 
     <a class="navbar-brand" href="index.php">
       <img src="img/logo.png" alt="Logo" class="logo" width="100px">
     </a>
 
     <a class="navbar-brand" href="index.php">
-      <h1 class="color-red">DrinkToast</h1>
+      <h1 class="color-red title">DrinkToast</h1>
     </a>
+
   </nav>
 
   <div class="container mt-4">
@@ -32,71 +34,109 @@
       <h1 class="color-red">Drink Duel</h1>
     </header>
     <main>
-      <div class="container mt-4">
-        <div class="row mb-4">
-          <div class="col-md-6">
-            <div class="left-image">Image gauche</div>
-          </div>
-          <div class="col-md-6">
-            <div class="right-image">Image droite</div>
+
+      <!-- Image et description du jeu -->
+      <div class="container mt-4 mb-4">
+        <div class="card rounded">
+          <div class="row no-gutters">
+            <div class="col-md-6">
+              <img src="img/drinkduel.png" alt="Image de duel" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+              <div class="card-body">
+                <p class="card-text text-justify">Bienvenue sur DrinkToast, le jeu qui vous permet de vous amuser avec vos amis tout en buvant un coup !<br> Pour jouer, il vous suffit de créer un lobby et d'inviter vos amis à vous rejoindre. Une fois que tout le monde est prêt, vous pouvez lancer la partie et commencer à jouer !</p>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <!-- Formulaire pour enregistrer le pseudo -->
-        <form id="pseudo-form" method="post" class="mb-4">
+
+      <!-- Formulaire pour enregistrer le pseudo -->
+      <form id="pseudo-form" method="post" class="mb-4">
+        <div class="form-group">
+          <label for="pseudo">Pseudo :</label>
+          <input type="text" id="pseudo" name="pseudo" class="form-control" required maxlength="15">
+        </div>
+        <button type="submit" class="btn btn-primary">Enregistrer le pseudo!</button>
+      </form>
+
+
+      <!-- Création et gestion de lobby -->
+      <div id="lobby-creation" class="mb-4">
+        <h2>Créer un nouveau lobby</h2>
+
+        <form id="create-lobby-form" method="post" class="mb-4">
           <div class="form-group">
-            <label for="pseudo">Pseudo :</label>
-            <input type="text" id="pseudo" name="pseudo" class="form-control" required maxlength="15">
+            <label for="lobby-name">Nom du lobby :</label>
+            <input type="text" id="lobby-name" name="lobby-name" class="form-control" required autocomplete="off" maxlength="20">
           </div>
-          <button type="submit" class="btn btn-primary">Enregistrer le pseudo!</button>
+          <div class="form-group">
+            <label for="lobby-password">Mot de passe du lobby (laisser vide pour un lobby public) :</label>
+            <input type="password" id="lobby-password" name="lobby-password" class="form-control">
+          </div>
+          <button type="submit" class="btn btn-primary">Créer le lobby</button>
         </form>
 
-
-        <!-- Création et gestion de lobby -->
-        <div id="lobby-creation" class="mb-4">
-          <h2>Créer un nouveau lobby</h2>
-
-          <form id="create-lobby-form" method="post" class="mb-4">
-            <div class="form-group">
-              <label for="lobby-name">Nom du lobby :</label>
-              <input type="text" id="lobby-name" name="lobby-name" class="form-control" required autocomplete="off" maxlength="20">
-            </div>
-            <div class="form-group">
-              <label for="lobby-password">Mot de passe du lobby (laisser vide pour un lobby public) :</label>
-              <input type="password" id="lobby-password" name="lobby-password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-primary">Créer le lobby</button>
-          </form>
-
-          <h2>Rejoindre un lobby existant :</h2>
-          <ul id="lobby-list" class="list-group"></ul>
-        </div>
-
-        <!-- Informations sur le lobby -->
-        <div id="lobby-info" style="display: none;">
-          <h1>Lobby: <span id="lobby-name-display"></span></h1>
-          <h3 id="player-count">Joueurs:</h3>
-          <ul id="player-list" class="list-group">
-            <!-- La liste des joueurs sera ajoutée ici par le script JavaScript -->
-          </ul>
-          <button type="button" id="start-game" class="btn btn-success" onclick="startGame()">Lancer la partie</button>
-          <p id="error-message" class="text-danger"></p>
-          <div id="game-rules" class="mt-4">
-            <img src="img/regle.png" alt="voici les petites régles d'amour" id="rules-image" class="img-fluid">
-            <h3>Règles du jeu :</h3>
-            <!-- Règles du jeu ici -->
-          </div>
-        </div>
-        <p class="warning-text">L'abus d'alcool est dangereux pour la santé, à consommer avec modération. <br> Vous pouvez jouer sans boire!!</p>
+        <h2>Rejoindre un lobby existant :</h2>
+        <ul id="lobby-list" class="list-group"></ul>
       </div>
-    </main>
 
-
-    <footer class="text-center mt-4">
-      <p class="color-blue">&copy; 2023 DrinkToast</p>
-      <p class="text-muted" style="font-size: small;">Version 1.0.0</p>
-    </footer>
+      <!-- Informations sur le lobby -->
+      <div id="lobby-info" style="display: none;">
+        <h1>Lobby: <span id="lobby-name-display"></span></h1>
+        <h3 id="player-count">Joueurs:</h3>
+        <ul id="player-list" class="list-group">
+          <!-- La liste des joueurs sera ajoutée ici par le script JavaScript -->
+        </ul>
+        <button type="button" id="start-game" class="btn btn-success" onclick="startGame()">Lancer la partie</button>
+        <p id="error-message" class="text-danger"></p>
+        <div id="game-rules" class="mt-4">
+          <img src="img/regle.png" alt="voici les petites régles d'amour" id="rules-image" class="img-fluid">
+          <h3>Règles du jeu :</h3>
+          <!-- Règles du jeu ici -->
+        </div>
+      </div>
+      <p class="warning-text">L'abus d'alcool est dangereux pour la santé, à consommer avec modération. <br> Vous pouvez jouer sans boire!!</p>
   </div>
+  </main>
+
+
+
+  </div>
+
+  <footer class="bg-dark text-white pt-4 pb-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <!-- Section À propos -->
+            <div class="col-md-4 mb-3">
+                <h5>À propos de DrinkToast</h5>
+            </div>
+
+            <!-- Section Liens utiles -->
+            <div class="col-md-4 mb-3">
+                <h5>Liens utiles</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Politique de confidentialité</a></li>
+                    <li><a href="#" class="text-white">Termes et conditions</a></li>
+                    <li><a href="#" class="text-white">FAQ</a></li>
+                </ul>
+            </div>
+
+            <!-- Section Contact et réseaux sociaux -->
+            <div class="col-md-4 mb-3">
+                <h5>Contactez-nous</h5>
+                <a href="mailto:drinktoastgame@gmail.com">Email : drinktoastgame@gmail.com</a>
+            </div>
+        </div>
+
+        <div class="border-top pt-3 mt-3">
+            <p class="mb-0">&copy; 2023 DrinkToast</p>
+            <p style="font-size: small;">Version 1.0.0</p>
+        </div>
+    </div>
+</footer>
+
 
   <!-- Bootstrap JS et jQuery -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
